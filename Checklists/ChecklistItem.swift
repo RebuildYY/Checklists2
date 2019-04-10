6930114f50ed4e180f4d6579852fa6840a31893f
LeeYunSeok <dbstjrrl803@naver.com>
2019-04-10 15:44:20 +0900

    tableView3

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,19 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by LeeYunSeok on 10/04/2019.
+//  Copyright © 2019 LeeYunSeok. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+    
+    /* For toggling */
+    func toggleChecked() {
+        checked = !checked
+    }
+}
