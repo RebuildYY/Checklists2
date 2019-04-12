b1c51afd653817ad657bded7eb1fa06173606a08
LeeYunSeok <dbstjrrl803@naver.com>
2019-04-12 10:27:55 +0900

    commit listDataD

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -11,16 +11,26 @@ import UIKit
 class AllListsViewController: UITableViewController {
     /* Cell id */
     let cellIdentifier = "ChecklistCell"
+    var lists = [Checklist]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
         
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+        /* Dummy data */
+        // Add placeholder data
+        var list = Checklist(name: "Birthdays")
+        lists.append(list)
         
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        list = Checklist(name: "Groceries")
+        lists.append(list)
+        
+        list = Checklist(name: "Cool Apps")
+        lists.append(list)
+        
+        list = Checklist(name: "To Do")
+        lists.append(list)
+
     }
     
     // MARK: - Table view data source
@@ -32,7 +42,7 @@ class AllListsViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 3
+        return lists.count
     }
     
     
@@ -40,13 +50,25 @@ class AllListsViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
         
         // Configure the cell...
-        cell.textLabel!.text="List\(indexPath.row)"
+        let checklist = lists[indexPath.row]
+        cell.textLabel!.text = checklist.name
+        cell.accessoryType = .detailDisclosureButton
         return cell
     }
     
     /* didSelectRowAt */
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "ShowChecklist", sender: nil)
+        /* Send checklit into ShowChecklist id Segue */
+        let checklist = lists[indexPath.row]
+        performSegue(withIdentifier: "ShowChecklist", sender: checklist)
+        
     }
     
+    // MARK:- Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowChecklist" {
+            let controller = segue.destination as! ChecklistViewController
+            controller.checklist = sender as? Checklist
+        }
+    }
 }
---
+++ Checklists/Checklist.swift
@@ -0,0 +1,18 @@
+//
+//  Checklist.swift
+//  Checklists
+//
+//  Created by LeeYunSeok on 12/04/2019.
+//  Copyright © 2019 LeeYunSeok. All rights reserved.
+//
+
+import UIKit
+
+class Checklist: NSObject {
+    var name=""
+    
+    init(name: String) {
+        self.name = name
+        super.init()
+    }
+}
